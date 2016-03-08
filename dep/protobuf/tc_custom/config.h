d810a1870a17203984652507c1c9f5e43abaf87d
Shauren <shauren.trinity@gmail.com>
2016-03-08 20:54:12 +0100

    Add google protobuf (with minor hack)

---
+++ dep/protobuf/tc_custom/config.h
@@ -0,0 +1,7 @@
+/* Do not define anything - we want to avoid having to maintain complex custom build scripts */
+
+/* define if the compiler has hash_map */
+//#define HAVE_HASH_MAP 1
+
+/* define if the compiler has hash_set */
+//#define HAVE_HASH_SET 1
